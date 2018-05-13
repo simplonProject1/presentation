8b2aa0b5bfc50895c1a94813724de428b2ba1901
freeanke <freeanke@gmail.com>
2018-05-13 16:39:22 +0200

    cjllk

--- src/app/components/mp2019/mp2019.component.ts
+++ src/app/components/mp2019/mp2019.component.ts
@@ -1,14 +1,37 @@
-import { Component } from '@angular/core';
+import {Component, OnInit, ViewChild} from '@angular/core';
 import { ToggleDisplayService } from '../../services/toggle-display.service';
+import {PointObject} from '../../objects/point.object';
 
 @Component({
   selector: 'app-mp2019',
   templateUrl: './mp2019.component.html',
   styleUrls: ['./mp2019.component.css']
 })
-export class Mp2019Component {
+export class Mp2019Component implements OnInit {
+
+  @ViewChild('arrow')
+  canvas: any;
+
+  displayMP: DisplayMP;
 
   constructor(public toggleDisplayService: ToggleDisplayService) {}
+
+  createPoint(beginX, beginY) {
+    const point = new PointObject();
+    point.beginX = beginX;
+    point.beginY = beginY;
+    return point;
+  }
+
+  ngOnInit() {
+    this.displayMP = new DisplayMP();
+    this.displayMP.red = false;
+    this.displayMP.green = false;
+  }
+
+  toggleColorMP2019(color: string): void {
+    this.displayMP[color] = !this.displayMP[color];
+  }
 
   toggleDisplay(display: string): void {
     this.toggleDisplayService.setDisplay(display);
@@ -17,4 +40,10 @@ export class Mp2019Component {
   get year(): number {
     return this.toggleDisplayService.year;
   }
+
+}
+
+export class DisplayMP {
+  red: boolean;
+  green: boolean;
 }
