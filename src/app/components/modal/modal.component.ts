7743dbe08250bdd94418473cd472fb64e43026ac
freeanke <freeanke@gmail.com>
2018-05-13 11:38:24 +0200

    klsfldkflk

--- src/app/components/modal/modal.component.ts
+++ src/app/components/modal/modal.component.ts
@@ -1,7 +1,4 @@
 import { Component, OnInit, Input } from '@angular/core';
-import {NgbModal, NgbActiveModal} from '@ng-bootstrap/ng-bootstrap';
-import {MatrixComponent} from './content/matrix/matrix.component';
-import {ApptioComponent} from '../apptio/apptio.component';
 
 @Component({
   selector: 'app-modal',
@@ -13,23 +10,7 @@ export class ModalComponent implements OnInit {
   @Input()
   content: string;
 
-  constructor(private modalService: NgbModal) {}
-
-  // open() {
-  //   switch (this.content) {
-  //     case 'matrix':
-  //       const modalRef = this.modalService.open(MatrixComponent);
-  //       modalRef.componentInstance.name = 'World';
-  //       break;
-  //     case ('apptio'):
-  //       const modalRef = this.modalService.open(ApptioComponent);
-  //       modalRef.componentInstance.name = 'World';
-  //       break;
-  //     default:
-  //       break;
-  //   }
-  //
-  // }
+  constructor() {}
 
   ngOnInit() {
   }
